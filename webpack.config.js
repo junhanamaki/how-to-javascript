f9d18456aa58ee6a651d2e5ffaeb229f21d2ffe6
junhanamaki <jun.hanamaki@gmail.com>
2016-01-28 11:02:22 +0000

    config

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,14 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: path.resolve(__dirname, 'dist/bundle.js')
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
   }
 };
