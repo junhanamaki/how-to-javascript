3e4480ceea0d92971de7b52ebf64265d9cbdaddc
junhanamaki <jun.hanamaki@gmail.com>
2016-01-31 13:59:41 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const _ = require('lodash');
 
-window.alert(_.capitalize('hello world!')); // eslint-disable-line no-alert
+window.alert(_.capitalize('hello universe!')); // eslint-disable-line no-alert
