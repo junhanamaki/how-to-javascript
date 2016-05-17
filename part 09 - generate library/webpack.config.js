a481bd1e81640f676f37b43d47a0fd48716cbc2b
Jun Hanamaki <jun.hanamaki@gmail.com>
2016-05-17 18:31:54 +0100

    configurations

--- part 09 - generate library/webpack.config.js
+++ part 09 - generate library/webpack.config.js
@@ -1,26 +1,61 @@
+const webpack = require('webpack');
 const resolve = require('path').resolve;
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
+const NODE_ENV = process.env.NODE_ENV;
 
-module.exports = {
-  entry: resolve(__dirname, 'src/index.js'),
-  output: {
-    path: resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    library: 'Popup'
-  },
-  module: {
-    preLoaders: [
-      { test: /\.js$/, loader: 'eslint', exclude: /node_modules/ }
-    ],
-    loaders: PRODUCTION
-      ? [
+if (NODE_ENV === 'dev_bundle') {
+  module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'popup.js',
+      library: 'Popup'
+    },
+    module: {
+      loaders: [
         { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
       ]
-      : [
+    },
+    externals: {
+      lodash: 'lodash'
+    },
+    devtool: 'source-map'
+  };
+} else if (NODE_ENV === 'prod_bundle') {
+  module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'popup.min.js',
+      library: 'Popup'
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
+      ]
+    },
+    externals: {
+      lodash: 'lodash'
+    },
+    plugins: [new webpack.optimize.UglifyJsPlugin()]
+  };
+} else {
+  module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'popup.js',
+      library: 'Popup'
+    },
+    module: {
+      preLoaders: [
+        { test: /\.js$/, loader: 'eslint', exclude: /node_modules/ }
+      ],
+      loaders: [
         { test: /\.js$/, loader: 'babel', exclude: /node_modules|src/ },
         { test: /\.js$/, loader: 'babel-istanbul', exclude: /node_modules|test/ }
       ]
-  },
-  devtool: PRODUCTION ? '' : 'inline-source-map'
-};
+    },
+    devtool: 'inline-source-map'
+  };
+}
